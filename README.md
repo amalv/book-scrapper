71dcff1c0cd0d94f499de74e92c8e73cde505450
amalv <1252707+amalv@users.noreply.github.com>
2023-12-18 16:55:46 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Book Scraper
+
+A simple web scraper for books.
+
+## Usage
+
+1. Install dependencies with `npm install`.
+2. Run the script with `npm run scrape`.
+
+This will create a `books.json` file with the scraped data.
---
+++ src/scraper.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { JSDOM } from "jsdom";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function scrapeBooks(url: string) {
+  const response = await axios.get(url);
+  const dom = new JSDOM(response.data);
+
+  const bookElements = dom.window.document.querySelectorAll(
+    "tr[itemtype='http://schema.org/Book']"
+  );
+  const books = Array.from(bookElements).map((bookElement) => {
+    const titleElement = bookElement.querySelector(".bookTitle span");
+    const authorElement = bookElement.querySelector(".authorName span");
+    const ratingElement = bookElement.querySelector(".minirating");
+
+    const title = titleElement ? titleElement.textContent : null;
+    const author = authorElement ? authorElement.textContent : null;
+    const image = ""; // Set image to an empty string
+    let rating = null;
+    let ratingsCount = null;
+    if (ratingElement && ratingElement.textContent) {
+      const ratingText = ratingElement.textContent;
+      const match = ratingText.match(
+        /(\d+\.\d+) avg rating — (\d+(?:,\d+)*) ratings/
+      );
+      if (match) {
+        rating = Math.round(parseFloat(match[1]) * 20); // Convert rating to 0-100 scale and round to an integer
+        ratingsCount = parseInt(match[2].replace(/,/g, ""));
+      }
+    }
+
+    return { title, author, image, rating, ratingsCount };
+  });
+
+  return books;
+}
+
+async function main() {
+  const url = process.env.BOOKS_URL;
+  if (!url) {
+    throw new Error("BOOKS_URL must be set in the .env file");
+  }
+  const books = await scrapeBooks(url);
+  fs.writeFileSync("output.json", JSON.stringify(books, null, 2));
+}
+
+main();
